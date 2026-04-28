6bcf3315fdeff3c81c192fe70d2addff68bb581a
onnanna <hakkanna@cubbli24-gold.cs.helsinki.fi>
2026-04-28 16:59:22 +0300

    uusi korjausyritys databaselle

--- src/database_connection.py
+++ src/database_connection.py
@@ -1,6 +1,8 @@
+from pathlib import Path
 import sqlite3
 from config import DATABASE_FILE_PATH
 
+Path(DATABASE_FILE_PATH).parent.mkdir(parents=True, exist_ok=True)
 connection = sqlite3.connect(DATABASE_FILE_PATH)
 connection.row_factory = sqlite3.Row
 
